a780bad9b73128ba9051bed0f7a2928b5f6026c3
Matt McCormick (thewtex) <matt@mmmccormick.com>
2009-11-04 14:32:37 -0600

    redefine itkRThetaTransform.h

--- source/itkRThetaTransform.h
+++ source/itkRThetaTransform.h
@@ -8,72 +8,162 @@
 #ifndef __itkRThetaTransform_h
 #define __itkRThetaTransform_h
 
-#include "itkImageToImageFilter.h"
+#include "itkTransform.h"
 
 namespace itk
 {
 
-/** @brief Scan convert an ultrasound image from a curvilinear array.
+/** @brief Transform for an image that was actually acquired in (R, Theta)
+ * space.  E.g., scan convert an ultrasound image from a curvilinear array.
  * 
- *Requires that the MetaDataDictionary contains entries for:
+ *  Properties:
+ *  RDirection
+ *    The direction in the input image assumed to correspond to the radial
+ *    component.
  *
- *  Radius
- *    The distance from the center of rotation to the first sample. type: double
+ *  ThetaDirection
+ *    The direction in the input image assumed to correspond to the angular
+ *    component.
  *
- *  Theta
- *    A vector containing the angles in radians of each scan line.  The length of
- *    this vector should be the same as the extent in the Direction property.
- *    type: std::vector< double >
+ * Parameters include:
+ *
+ *  Rmin
+ *    The distance from the center of rotation to the first sample in the
+ *    RDirection. type: double
+ *
+ *  Rmax
+ *    Distance in physical units to the last point in the RDirection.  Rmin +
+ *    size[RDirection] * spacing[RDirection]
+ *
+ *  MaxAbsTheta
+ *    Maximum absolute value of all angles.  Theta is in radians.
+ *
+ *  Thetamin
+ *    Minimum angle ( angle of the first line );
+ *
+ *  SpacingThetaOverDeltaTheta
+ *    Since the input image was acually in (R,Theta) space, a spacing had to be
+ *    assumed in the Theta direction to make the image.  This is that spacing
+ *    divided by the step in angle between lines in the theta direction.
+ *
  */
 
-template < class TInputImage, class TOutputImage >
+template < class TScalarType, unsigned int NDimensions=3 >
 class ITK_EXPORT RThetaTransform :
-  public ImageToImageFilter< TInputImage, TOutputImage >
+  public Transform< TScalarType, NDimensions, NDimensions >
 {
 public:
   /** Standard "Self" typedef.   */
   typedef RThetaTransform Self;
 
-  /** The type of input image.   */
-  typedef TInputImage InputImageType;
-
-  /** Dimension of the input and output images. */
-  itkStaticConstMacro ( ImageDimension, unsigned int,
-                       TInputImage::ImageDimension );
-  
-  /** Typedef support for the input image scalar value type. */
-  typedef typename InputImageType::PixelType InputPixelType;
-
-  /** The type of output image.   */
-  typedef TOutputImage OutputImageType;
-
-  /** Typedef support for the output image scalar value type. */
-  typedef typename OutputImageType::PixelType OutputPixelType;
-
-  /** Other convenient typedefs   */
-  typedef typename InputImageType::RegionType InputRegionType;
-  typedef typename InputImageType::SizeType   InputSizeType;
-  typedef typename InputImageType::IndexType  InputIndexType;
-  
   /** Standard super class typedef support. */
-  typedef ImageToImageFilter< InputImageType, OutputImageType > Superclass;
-
+  typedef Transform< TScalarType, NDimensions, NDimensions > Superclass;
 
   /** Smart pointer typedef support  */
   typedef SmartPointer< Self > Pointer;
   typedef SmartPointer< const Self > ConstPointer;
 
   /** Run-time type information (and related methods) */
-  itkTypeMacro( RThetaTransform, ImageToImageFilter );
+  itkTypeMacro( RThetaTransform, Transform );
   
   /** Method for creation through the object factory. */
   itkNewMacro( Self );
 
-  /** Get the direction in which the filter is to be applied. */
-  itkGetMacro(Direction, unsigned int);
+  /** Type of the input parameters. */
+  typedef  TScalarType     ScalarType;
 
-  /** Set the direction in which the filter is to be applied. */
-  itkSetMacro(Direction, unsigned int);
+  /** Standard parameters container. */
+  typedef typename Superclass::ParametersType ParametersType;
+
+  /** Standard coordinate point type for this class */
+  typedef Point<TScalarType,
+                itkGetStaticConstMacro(InputSpaceDimension)> InputPointType;
+  typedef Point<TScalarType,
+                itkGetStaticConstMacro(OutputSpaceDimension)> OutputPointType;
+
+  /** Standard vector type for this class. */
+  typedef Vector<TScalarType,
+                 itkGetStaticConstMacro(SpaceDimension)> InputVectorType;
+  typedef Vector<TScalarType,
+                 itkGetStaticConstMacro(SpaceDimension)> OutputVectorType;
+
+  /** Standard covariant vector type for this class. */
+  typedef CovariantVector<TScalarType,
+             itkGetStaticConstMacro(SpaceDimension)> InputCovariantVectorType;
+  typedef CovariantVector<TScalarType,
+            itkGetStaticConstMacro(SpaceDimension)> OutputCovariantVectorType;
+  
+  /** Standard vnl_vector type for this class. */
+  typedef vnl_vector_fixed<TScalarType,
+                   itkGetStaticConstMacro(SpaceDimension)> InputVnlVectorType;
+  typedef vnl_vector_fixed<TScalarType,
+                  itkGetStaticConstMacro(SpaceDimension)> OutputVnlVectorType;
+  
+  /**  Method to transform a point. */
+  virtual OutputPointType TransformPoint(const InputPointType  &point ) const;
+
+  /** Method to transform a vector - 
+   *  not applicable for this type of transform. */
+  virtual OutputVectorType TransformVector(const InputVectorType &) const
+    { 
+    itkExceptionMacro(<< "Method not applicable for deformable transform." );
+    return OutputVectorType(); 
+    }
+
+  /** Method to transform a vnl_vector - 
+   *  not applicable for this type of transform */
+  virtual OutputVnlVectorType TransformVector(const InputVnlVectorType &) const
+    { 
+    itkExceptionMacro(<< "Method not applicable for deformable transform. ");
+    return OutputVnlVectorType(); 
+    }
+
+  /** Method to transform a CovariantVector - 
+   *  not applicable for this type of transform */
+  virtual OutputCovariantVectorType TransformCovariantVector(
+    const InputCovariantVectorType &) const
+    { 
+    itkExceptionMacro(<< "Method not applicable for deformable transfrom. ");
+    return OutputCovariantVectorType(); 
+    } 
+
+  /** Indicates that this transform is linear. That is, given two
+   * points P and Q, and scalar coefficients a and b, then
+   *
+   *           T( a*P + b*Q ) = a * T(P) + b * T(Q)
+   */
+  virtual bool IsLinear() const { return false; }
+
+ /**  Rmin
+ *    The distance from the center of rotation to the first sample in the
+ *    RDirection. type: double */
+  virtual void SetRmin( const double& Rmin )
+    { 
+    m_Parameters[0] = Rmin;
+    this->Modified();
+    }
+
+ /**  Rmax
+ *    Distance in physical units to the last point in the RDirection.  Rmin +
+ *    size[RDirection] * spacing[RDirection] */
+  virtual void SetRMax( const double& Rmax )
+    {
+    m_Parameters[1] = Rmax;
+    this->Modified();
+    }
+
+  /** SpacingTheta
+   *	The spacing in the ThetaDirection.  This must be set before ThetaArray.
+   *	*/
+  itkSetMacro( SpacingTheta, double );
+
+  /** ThetaArray
+   *	An itk::Array<double> who elements correspond to the angle in radians of
+   *	each element in the ThetaDirection.  ThetaArray.size() should be the
+   *	same as the size of the image in the ThetaDirection.  The MaxAbsTheta,
+   *	Thetamin, and SpacingThetaOverDeltaTheta Parameters are defined after
+   *	calling this method.  SetSpacingTheta() must be called before this. */
+  virtual void SetThetaArray( const itk::Array< double >& theta );
 
 protected:
   RThetaTransform();
@@ -81,18 +171,7 @@ protected:
 
   void PrintSelf( std::ostream& os, Indent indent ) const;
 
-  /** Standard process object method. */
-  virtual void GenerateOutputInformation(void);
-
-  /** Standard process object method. */
-  virtual void GenerateInputRequestedRegion();
-
-  /** Standard process object method. */
-  virtual void GenerateData();
-
-  /** Direction in which the filter is to be applied.
-   * This should be in the range [0,ImageDimension-1]. */
-  unsigned int m_Direction;
+  double m_SpacingTheta;
 
 private:
   RThetaTransform( const Self& ); // purposely not implemented
--- source/itkRThetaTransform.txx
+++ source/itkRThetaTransform.txx
@@ -9,66 +9,14 @@
 namespace itk
 {
 
-template < class TInputImage, class TOutputImage >
+template < class TScalarType, class TOutputImage >
 RThetaTransform< TInputImage, TOutputImage >
 ::RThetaTransform():
-  m_Direction(1)
+  Superclass( SpaceDimension, 5 ),
+  m_SpacingTheta( 0.0 )
 {
 }
 
-
-template < class TInputImage, class TOutputImage >
-void
-RThetaTransform< TInputImage, TOutputImage >
-::PrintSelf( std::ostream& os, Indent indent ) const
-{
-  //! @todo define me
-}
-
-
-template < class TInputImage, class TOutputImage >
-void
-RThetaTransform< TInputImage, TOutputImage >
-::GenerateOutputInformation()
-{
-  this->Superclass::GenerateOutputInformation();
-
-  const MetaDataDictionary& dict = this->GetMetaDataDictionary();
-  if( !dict.HasKey( "Radius" ) )
-    {
-    itkExceptionMacro( "Could not find 'Radius' MetaDataDictionary entry." );
-    }
-  const double radius = (*( dynamic_cast< const itk::MetaDataObject< double >* >( dict["Radius"] ))).GetMetaDataObjectValue();
-
-  if( !dict.HasKey( "Theta" ) )
-    {
-    itkExceptionMacro( "Could not find 'Theta' MetaDataDictionary entry." );
-    }
-  const std::vector< double >& theta = (*( dynamic_cast< const itk::MetaDataObject< std::vector< double > >* >( dict["Theta"] ))).GetMetaDataObjectValue();
-  
-  unsigned int numLines = this->GetInput()->GetLargestPossibleRegion().GetSize()[this->m_Direction];
-  for( unsigned int i = 0; i < numLines; i++ )
-    std::cout << theta[i] << std::endl;
-  
-  //! @todo define me
-}
-
-
-template < class TInputImage, class TOutputImage >
-void
-RThetaTransform< TInputImage, TOutputImage >
-::GenerateInputRequestedRegion()
-{
-  //! @todo define me
-}
-
-template < class TInputImage, class TOutputImage >
-void
-RThetaTransform< TInputImage, TOutputImage >
-::GenerateData()
-{
-  //! @todo define me
-}
 
 }
 
