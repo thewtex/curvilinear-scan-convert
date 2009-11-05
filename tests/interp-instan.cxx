24d4e05e62957b3a19aa83e61beb0db310b02aa7
Matt McCormick (thewtex) <matt@mmmccormick.com>
2009-11-05 11:10:26 -0600

    make so can use installed visualsonics

--- tests/interp-instan.cxx
+++ tests/interp-instan.cxx
@@ -2,8 +2,14 @@
 
 #include "itkImage.h"
 
+#include "itkFixedArray.h"
+#include "itkMatrix.h"
+
 typedef unsigned short OutputPixelType;
 const unsigned int Dimension = 3;
 typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
 
 template class itk::NearestNeighborInterpolateImageFunction< OutputImageType, double >;
+
+template class itk::FixedArray< double, Dimension >;
+template class itk::Matrix< double, Dimension, Dimension >;
